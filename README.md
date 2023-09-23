95dfbc5ee188998c4a3db6b1fbc34b8ae57d44a8
yamake <yamake@gmail.com>
2023-09-23 11:59:59 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 参考
+
+https://qiita.com/anqooqie/items/92005e337a0d2569bdbd#%E3%83%AC%E3%83%BC%E3%83%88%E3%81%AE%E7%AE%97%E5%87%BA
---
+++ rating_calculator.py
@@ -0,0 +1,62 @@
+import requests
+import datetime
+import math
+import sys
+
+def time_parser(time: str):
+    year, month, day = time.split("-")
+    return int(year), int(month), int(day.split("T")[0])
+
+def is_in_period(year, month, day, period):
+    now = datetime.datetime.now()
+    last = now + datetime.timedelta(weeks = -period*4)
+    # print(last)
+    if year > last.year:
+        return True
+    elif year < last.year:
+        return False
+    if month > last.month:
+        return True
+    elif month < last.month:
+        return False
+    if day >= last.day:
+        return True
+    else:
+        return False
+
+def calculate_rating(rated_contests, period):
+    sm = 0
+    bunbo = 0
+    for i in range(len(rated_contests)):
+        performance = rated_contests[i][3]
+        # print(performance)
+        sm += math.pow(2,performance/800)*math.pow(0.9,i+1)
+        bunbo += math.pow(0.9,i+1)
+    rating = 800 * math.log(sm/bunbo, 2)
+    print(f"period = {period} month")
+    print(f"rated contest num = {len(rated_contests)}")
+    print(f"rating = {rating}")
+
+def main():
+    user_name = sys.argv[1]
+    period = int(sys.argv[2])
+    url = f"https://atcoder.jp/users/{user_name}/history/json"
+    response = requests.get(url)
+    user_info = response.json()
+    rated_contests = []
+    for info in user_info:
+        year, month, day = time_parser(info["EndTime"])
+        if info["IsRated"] and is_in_period(year, month, day, period):
+            rated_contests.append([year, month, day, info["Performance"]])
+    rated_contests.sort()
+    rated_contests.reverse()
+    if len(rated_contests) == 0:
+        print("no rated contest")
+        exit(0)
+    calculate_rating(rated_contests, period)
+    
+
+        
+
+if __name__ == "__main__":
+    main()
